65a12d1f97528122b9e23594477ffece2cadbb06
adarleika <adarleika@gmail.com>
2020-12-02 21:43:41 -0300

    modify component proyects, css, component skills

--- src/Components/Proyects/Proyects.jsx
+++ src/Components/Proyects/Proyects.jsx
@@ -6,22 +6,28 @@ import Social from '../../img/social-network.svg';
 
 const Proyects = () => {
     return (
+
         <div className="containerProyects">
             <div className="proyects">
 
                 <p className="p-proyects">Proyectos</p>
 
             </div>
-            <div className="containerImg">
+
+            <div className="containerTDC">
                 <img className="img-tdc" src={TDC} alt="TDC" />
             </div>
-            <div className="containerImg">
+            <div className="containerLovers">
                 <img className="img-tdc" src={Lovers} alt="Lovers" />
             </div>
-            <div className="containerImg">
+            <div className="containerSocial">
                 <img className="img-tdc" src={Social} alt="Social" />
             </div>
+
         </div>
+
+
+
     )
 }
 
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import './Skills.css';
 
 const Skills = () => {
-    return(
-        <div>
-            componente Skills
+    return (
+        <div className="containerSkills">
+            <div className="skills">
+
+                <p className="p-skills">Habilidades</p>
+
+            </div>
+
+            <div className="skills2">
+                <p className="p-2-skills">Las siguientes tecnologias son algunas <br />
+                de  las habilidades tecnicas que manejo <br />
+                en el ambito de desarrollo.</p>
+            </div>
+            
         </div>
     )
 }
