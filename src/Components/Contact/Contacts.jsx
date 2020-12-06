44499da73f7227d5145f4a21db4de24691adaf22
adarleika <adarleika@gmail.com>
2020-12-06 18:23:06 -0300

    components contacts and skills

--- src/Components/Contact/Contacts.jsx
+++ src/Components/Contact/Contacts.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import GITHUB from '../../img/github.svg';
+import LINKEDIN from '../../img/linkedin.svg';
 import './Contacts.css';
 
 const Contacts = () => {
-    return(
-        <div>
-            componente Contacts
+    return (
+        <div className="containerContacts">
+            <div className="contacts">
+
+                <p className="p-contacts">Contactame</p>
+
+            </div>
+
+            <div className="formContact">
+                <ul>
+                    <input className="nameContact" placeholder="Nombre Completo"></input>
+                    <input className="emailContact" placeholder="Correo Electronico"></input>
+                    <textarea className="msjContact" placeholder="Mensaje"></textarea>
+                </ul>
+            </div>
+            <div className="enviar">
+                <button className="btn-enviar">
+                    Enviar
+                </button>
+
+            </div>
+
+            <div className="iconImg">
+                <ul>
+                    <li>
+                        <img className="img-github2" src={GITHUB} alt="GITHUB" />
+                        <img className="img-linkedin" src={LINKEDIN} alt="LINKEDIN" />
+                    </li>
+                </ul>
+
+            </div>
+            <div className="containerFooter">
+                Copyright © 2020 | Adarleika Silva
+            </div>
         </div>
     )
 }
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import CSS from '../../img/css3.svg';
+import GIT from '../../img/git.svg';
+import GITHUB from '../../img/github.svg';
+import HTML from '../../img/html5.svg';
+import JS from '../../img/js.svg';
+import SERVERS from '../../img/servers.svg';
+import REACT from '../../img/react.svg';
+import BOOTSTRAP from '../../img/bootstrap.svg';
+import SCRUM from '../../img/scrum.svg';
+
 import './Skills.css';
+
 
 const Skills = () => {
     return (
@@ -15,7 +26,23 @@ const Skills = () => {
                 de  las habilidades tecnicas que manejo <br />
                 en el ambito de desarrollo.</p>
             </div>
-            
+
+            <div className="iconImg">
+                <ul>
+                    <li>
+                        <img className="img-css" src={CSS} alt="CSS" />
+                        <img className="img-git" src={GIT} alt="GIT" />
+                        <img className="img-github" src={GITHUB} alt="GITHUB" />
+                        <img className="img-html" src={HTML} alt="HTML" />
+                        <img className="img-js" src={JS} alt="JS" />
+                        <img className="img-servers" src={SERVERS} alt="SERVERS" />
+                        <img className="img-react" src={REACT} alt="REACT" />
+                        <img className="img-bootstrap" src={BOOTSTRAP} alt="BOOTSTRAP" />
+                        <img className="img-scrum" src={SCRUM} alt="SCRUM" />
+                    </li>
+                </ul>
+            </div>
+
         </div>
     )
 }
